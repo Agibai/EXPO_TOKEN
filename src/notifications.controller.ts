fa01a4028824e95089a8484f6b08f193f1d63ce6
Agibai <agiikenjan95@gmail.com>
2025-09-17 21:52:51 +0500

    Create notifications.controller.ts

---
+++ src/notifications.controller.ts
@@ -0,0 +1,14 @@
+@Controller('notifications')
+export class NotificationsController {
+  constructor(private readonly notificationsService: NotificationsService) {}
+
+  @Get('me')
+  getMyNotifications(@Req() req) {
+    return this.notificationsService.getUserNotifications(req.user.userId);
+  }
+
+  @Post(':id/read')
+  markRead(@Param('id') id: string) {
+    return this.notificationsService.markAsRead(+id);
+  }
+}
