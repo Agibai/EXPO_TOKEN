c7eb1f7c37978f8050c7e6a379dee963d7a2a1ec
Agibai <agiikenjan95@gmail.com>
2025-09-17 18:40:06 +0500

    bookings.controller.ts

--- src/bookings/bookings.controller.ts
+++ src/bookings/bookings.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { BookingsService } from './bookings.service';
+import { JwtAuthGuard } from '../auth/jwt.guard';
+import { User } from '../auth/decorators/user.decorator';
+
 
 @Controller('bookings')
 export class BookingsController {
-  constructor(private bookingsService: BookingsService) {}
+constructor(private bookingsService: BookingsService) {}
 
-  @Post()
-  async create(@Body() dto: any) {
-    return this.bookingsService.createBooking(dto);
-  }
+
+@UseGuards(JwtAuthGuard)
+@Post()
+async create(@Body() dto: any, @User() user: any) {
+return this.bookingsService.createBooking({ ...dto, passengerId: user.userId });
+}
 }
